652fc89b084bf0f40790afafcf6fcbf9ec6b08c1
Adolfo Herrera <valherdolf@gmail.com>
2021-10-01 01:10:11 -0400

    Added a new div containing the todo list with some functionality

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+todoButton.addEventListener("click", addTodo);
+
+function addTodo() {
+    event.preventDefault();
+    
+    
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    const newTodo = document.createElement("li");
+    newTodo.textContent = todoInput.value;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+
+    //CHECK MARK BUTTON
+    const completedButton = document.createElement("button");
+    completedButton.textContent = "+";
+    completedButton.classList.add("complete-button");
+    todoDiv.appendChild(completedButton);
+
+    //CHECK TRASH BUTTON
+    const trashButton = document.createElement("button");
+    trashButton.textContent = "-";
+    trashButton.classList.add("trash-button");
+    todoDiv.appendChild(trashButton);
+
+    //APPEND TO LIST
+    todoList.appendChild(todoDiv);
+}
+
+
+
+
+
+
+
